0934ebe363e9c499a8773bf79d04a44a8c1a21a3
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-08-12 00:25:18 -0400

    initial code

---
+++ src/components/App/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './styles.css'
+import HomePage from "../HomePage";
+import Login from "../Login";
+import Register from "../Register";
+import Dashboard from "../Dashboard";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const theme = createMuiTheme();
+
+export default function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+  );
+}

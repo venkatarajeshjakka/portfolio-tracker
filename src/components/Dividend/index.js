614cd0de8c0e3f0d25b5057f969d1691c1e92d97
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-08-18 21:48:43 -0400

    added divided

--- src/components/Dividend/index.js
+++ src/components/Dividend/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import {
+  Typography,
+  Grid,
+  Container,
+  Paper
+} from "@material-ui/core";
 
-const Dividend = () =>{
 
-    return(<>hello Dividend page</>)
-}
+const styles = theme => ({
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+      },
+});
+const Dividend = ({ classes }) => {
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9} >
+                <Paper>
+                    <Typography component="h5" >
+                        Hello
+                    </Typography>
+                </Paper>
+            </Grid>
 
-export default Dividend;
+        </Grid>
+    </Container>
+    
+  );
+};
+
+export default withStyles(styles)(Dividend);

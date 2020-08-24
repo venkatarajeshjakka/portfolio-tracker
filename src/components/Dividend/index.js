24c576c1797dcd371303cbb6fef802b0d188595f
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-08-24 13:54:28 -0400

    added modal popup

--- src/components/Dividend/index.js
+++ src/components/Dividend/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import { Paper } from "@material-ui/core";
+import {
+  Paper,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "@material-ui/core";
 import DividendForm from "./DividendForm";
 import PageHeader from "../shared/PageHeader";
 import { AttachMoneyRounded as AttachMoneyRoundedIcon } from "@material-ui/icons";
+import { Button } from "../Controls";
+
 const styles = theme => ({
   pageContent: {
     margin: theme.spacing(5),
@@ -11,6 +19,16 @@ const styles = theme => ({
   }
 });
 const Dividend = ({ classes }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <PageHeader
@@ -18,7 +36,28 @@ const Dividend = ({ classes }) => {
         subTitle="Earning from investment"
         icon={<AttachMoneyRoundedIcon fontSize="large" />}
       />
-      <DividendForm />
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={handleClickOpen}
+        text={"Open"}
+      />
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here.
+            We will send updates occasionally.
+          </DialogContentText>
+          <DividendForm />
+        </DialogContent>
+      </Dialog>
+
       <Paper className="classes.pageContent"></Paper>
     </>
   );

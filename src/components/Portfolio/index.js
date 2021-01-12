b4067e0060402b1cabbc26e048eb837b5427a48f
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2021-01-12 15:03:47 -0500

    added top movers serction

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { Grid, Box, CircularProgress, Paper } from "@material-ui/core";
+import { Grid, Box, Paper, Typography, Divider } from "@material-ui/core";
 import PageHeader from "../shared/PageHeader";
 import { withStyles } from "@material-ui/core/styles";
 import { Link, withRouter } from "react-router-dom";
@@ -17,7 +17,7 @@ import DisplayItemSection from "./DisplayItemSection";
 import { formatCurrency } from "../../extensions/Formatters";
 import { Red, Green } from "../../color";
 import SectorComposition from "./SectorComposition";
-import StockComposition from './StockComposition'
+import StockComposition from "./StockComposition";
 import ColoredCircularProgress from "../UtilityComponents/ColoredCircularProgress";
 const styles = theme => ({
   button: {
@@ -34,8 +34,12 @@ const styles = theme => ({
   productCard: {
     height: "100%"
   },
+  paperHeading: {
+    paddingLeft: theme.spacing(2)
+  },
   paper: {
-    marginTop: theme.spacing(3),
+    minHeight: "80%",
+    marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
     padding: theme.spacing(2),
     boxShadow: "0 8px 24px 0 rgba(0,0,0,0.12)",
@@ -55,6 +59,67 @@ const styles = theme => ({
     paddingRight: theme.spacing(1)
   }
 });
+
+const MoverItem = ({
+  stockName,
+  ltp,
+  dailyGain,
+  changePercentage,
+  redClass,
+  greenClass,
+  priceClass,
+  headingClass
+}) => {
+  return (
+    <Grid item>
+      <Typography
+        className={headingClass}
+        gutterBottom
+        variant="h6"
+      >
+        {stockName}
+      </Typography>
+      <Typography
+        className={priceClass}
+        gutterBottom
+        variant="subtitle2"
+      >
+        {formatCurrency(ltp)}
+      </Typography>
+      <Typography
+        className={dailyGain > 0 ? greenClass : redClass}
+        gutterBottom
+        variant="subtitle2"
+      >
+        {`${formatCurrency(dailyGain)}(${changePercentage}%)`}
+      </Typography>
+    </Grid>
+  );
+};
+const DisplayTopMovers = ({ data, greenClass, redClass }) => {
+  const { topGainer, topLoser } = data.topMovers;
+  return (
+    <Grid container justify="space-between" alignItems="center" spacing={3}>
+      <MoverItem
+        stockName={topGainer.stockCode}
+        ltp={topGainer.ltp}
+        dailyGain={topGainer.dailyGain}
+        changePercentage={topGainer.changePercentage}
+        redClass={redClass}
+        greenClass={greenClass}
+      />
+      <Divider orientation="vertical" flexItem variant="middle" />
+      <MoverItem
+        stockName={topLoser.stockCode}
+        ltp={topLoser.ltp}
+        dailyGain={topLoser.dailyGain}
+        changePercentage={topLoser.changePercentage}
+        redClass={redClass}
+        greenClass={greenClass}
+      />
+    </Grid>
+  );
+};
 const DisplaySummary = ({ data, greenClass, redClass }) => {
   const {
     investment,
@@ -70,16 +135,16 @@ const DisplaySummary = ({ data, greenClass, redClass }) => {
         <DisplayItemSection
           variant={"h5"}
           label={"Investment"}
-          value={formatCurrency(investment)}
           alignItems={"center"}
+          value={formatCurrency(investment)}
         />
       </Grid>
       <Grid item>
         <DisplayItemSection
           variant={"h5"}
           label={"Current"}
-          value={formatCurrency(currentValue)}
           alignItems={"center"}
+          value={formatCurrency(currentValue)}
         />
       </Grid>
       <Grid item>
@@ -175,14 +240,35 @@ const Portfolio = ({ classes }) => {
             startIcon={<AddOutlinedIcon />}
           />
         </Grid>
-
-        <Paper className={classes.paper}>
-          <DisplaySummary
-            data={stockSummaryResponse}
-            redClass={classes.red}
-            greenClass={classes.green}
-          />
-        </Paper>
+        <Grid container justify="space-between" spacing={3}>
+          <Grid item sm={12} md={9} lg={9} xl={9}>
+            <Paper className={classes.paper}>
+              <Typography 
+              className={classes.paperHeading}
+              align={"left"} gutterBottom variant="h6">
+                Investment overview
+              </Typography>
+              <DisplaySummary
+                data={stockSummaryResponse}
+                redClass={classes.red}
+                greenClass={classes.green}
+              />
+            </Paper>
+          </Grid>
+          <Grid item sm={12} md={3} lg={3} xl={3} xs={12}>
+            <Paper className={classes.paper}>
+            <Typography
+              align={"left"} gutterBottom variant="h6">
+                Top movers
+              </Typography>
+              <DisplayTopMovers
+                data={stockSummaryResponse}
+                redClass={classes.red}
+                greenClass={classes.green}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
         <Grid container spacing={3}>
           <Grid item sm={12} md={9} lg={6} xl={6}>
             <SectorComposition data={stockSummaryResponse.sectorResponse} />

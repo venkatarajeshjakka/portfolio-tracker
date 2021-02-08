73672284afd07185a29c17d0ca4e3c387e2ded59
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2021-02-08 14:56:42 -0500

    Fixed top movers section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -88,24 +88,50 @@ const MoverItem = ({
     </Grid>
   );
 };
-const DisplayTopMovers = ({ data, greenClass, redClass }) => {
-  const { topGainer, topLoser } = data.topMovers;
+const DisplayTopLoser = ({ topLoser, redClass, greenClass }) => {
   return (
-    <Grid container justify="space-between" alignItems="center" spacing={3}>
-      <MoverItem
-        stockName={topGainer.stockCode}
-        ltp={topGainer.ltp}
-        dailyGain={topGainer.change}
-        changePercentage={topGainer.changePercentage}
-        redClass={redClass}
-        greenClass={greenClass}
-      />
+    <>
       <Divider orientation="vertical" flexItem variant="middle" />
       <MoverItem
         stockName={topLoser.stockCode}
         ltp={topLoser.ltp}
         dailyGain={topLoser.change}
         changePercentage={topLoser.changePercentage}
+        redClass={redClass}
+        greenClass={greenClass}
+      />
+    </>
+  );
+};
+const DisplayTopMovers = ({ data, greenClass, redClass }) => {
+  const { topGainer, topLoser } = data.topMovers;
+  if (topLoser.change < 0) {
+    return (
+      <Grid container justify="space-between" alignItems="center" spacing={3}>
+        <MoverItem
+          stockName={topGainer.stockCode}
+          ltp={topGainer.ltp}
+          dailyGain={topGainer.change}
+          changePercentage={topGainer.changePercentage}
+          redClass={redClass}
+          greenClass={greenClass}
+        />
+
+        <DisplayTopLoser
+          topLoser={topLoser}
+          redClass={redClass}
+          greenClass={greenClass}
+        />
+      </Grid>
+    );
+  }
+  return (
+    <Grid container alignItems="center" spacing={3}>
+      <MoverItem
+        stockName={topGainer.stockCode}
+        ltp={topGainer.ltp}
+        dailyGain={topGainer.change}
+        changePercentage={topGainer.changePercentage}
         redClass={redClass}
         greenClass={greenClass}
       />

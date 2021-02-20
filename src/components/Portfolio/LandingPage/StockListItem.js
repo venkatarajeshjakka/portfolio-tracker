a94bf09d6f70b1d024f350bd7c81462ebb71fc60
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2021-02-19 23:45:21 -0500

    fixed routing

--- src/components/Portfolio/LandingPage/StockListItem.js
+++ src/components/Portfolio/LandingPage/StockListItem.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Typography, IconButton, Grid, makeStyles } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { WorkOutlineOutlined as WorkOutlineOutlinedIcon } from "@material-ui/icons";
-
 import { Button as OutlineButton } from "../../Controls";
 import DisplayItemSection from "../DisplayItemSection";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { formatCurrency } from "../../../extensions/Formatters";
 const useStyles = makeStyles(theme => ({
   item: {
@@ -31,37 +30,43 @@ const useStyles = makeStyles(theme => ({
   title_tag: {
     color: "#ff5942",
     backgroundColor: "rgba(255,89,66,.1)",
-    padding: "4px 8px 4px",
+    padding: "0.25rem 0.5rem 0.25rem",
     margin: theme.spacing(1),
     textalign: "center",
-    borderRadius: "4px",
-    fontSize: "12px"
+    borderRadius: "0.25rem",
+    fontSize: "0.75rem"
   },
   tiltle_light: {
-    fontSize: "14px",
+    fontSize: "0.875rem",
     color: "#81878c"
   },
   bottom: {
     marginTop: theme.spacing(2),
-    paddingRight: theme.spacing(1),
+    paddingRight: theme.spacing(1)
   },
   heading: {
     paddingTop: theme.spacing(1),
     paddingLeft: theme.spacing(1)
   },
   price: {
-    fontSize: "18px",
+    fontSize: "0.875rem",
     color: "#040E5E",
     marginRight: theme.spacing(2),
     fontWeight: "400"
   },
   red: {
-    fontSize: "18px",
+    color: "#ff0000"
+  },
+  green: {
+    color: "#32cd32"
+  },
+  red_font: {
+    fontSize: "0.875rem",
     color: "#ff0000",
     paddingRight: theme.spacing(1)
   },
-  green: {
-    fontSize: "18px",
+  green_font: {
+    fontSize: "0.875rem",
     color: "#32cd32",
     fontWeight: "400",
     paddingRight: theme.spacing(1)
@@ -81,9 +86,15 @@ const useStyles = makeStyles(theme => ({
 }));
 const StockListItem = ({ data }) => {
   const classes = useStyles();
+  const history = useHistory();
 
   return (
-    <div className={classes.item}>
+    <div
+      onClick={() => {
+        history.push(`/position-details/${data.stockCode}`);
+      }}
+      className={classes.item}
+    >
       <Grid container justify="space-between" alignItems="center">
         <Grid item>
           <div className={classes.heading}>
@@ -98,7 +109,11 @@ const StockListItem = ({ data }) => {
             <div className={classes.price_section}>
               <span className={classes.price}>{formatCurrency(data.ltp)}</span>
 
-              <span className={data.change > 0 ? classes.green : classes.red}>
+              <span
+                className={
+                  data.change > 0 ? classes.green_font : classes.red_font
+                }
+              >
                 {`${formatCurrency(data.change)}(${data.changePercentage}%)`}
               </span>
             </div>
@@ -168,7 +183,7 @@ const StockListItem = ({ data }) => {
               color="primary"
             />
             <Typography color="textSecondary" display="inline" variant="body2">
-              {data.quantity}
+              {data.quantity} / {`Avg.${formatCurrency(data.avgPrice)}`}
             </Typography>
           </div>
         </Grid>
